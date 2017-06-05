4588c398f86e950d3b74477bf8d2b93cf63d4be4
Ivan Medina <medinadiazivan@gmail.com>
2017-06-05 10:39:06 -0500

    Geolocalización: id y JS

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var cargarPagina = function () {
+	$("#get-location").click(obtenerUbicacion);
+};
+
+var obtenerUbicacion = function (e) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(mostrarPosicion);
+	} else {
+		alert("Actualice su navegador");
+	}
+};
+
+var mostrarPosicion = function (posicion) {
+	console.log(posicion);
+	// alert("Latitud: " + posicion.coords.latitude);
+	// alert("Longitud: " + posicion.coords.longitude);
+
+	var coordenadas = {
+		lat: posicion.coords.latitude, 
+		lng: posicion.coords.longitude
+	};
+	mostrarMapa(coordenadas);
+};
+
+var mostrarMapa = function (coordenadas) {
+	var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17,
+      center: coordenadas
+    });
+    var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: map
+    });
+}
+
+$(document).ready(cargarPagina);
+
+
+
+
+
+
