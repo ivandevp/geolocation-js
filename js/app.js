9e5cca026a4a2c8c827a56d79f2157cfb0081eae
Ivan Medina <medinadiazivan@gmail.com>
2017-06-05 11:40:14 -0500

    Geolocalización: class y jQuery

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ var mostrarPosicion = function (posicion) {
 };
 
 var mostrarMapa = function (coordenadas) {
-	var map = new google.maps.Map($('#map')[0], {
+	var map = new google.maps.Map($('.map')[0], {
       zoom: 17,
       center: coordenadas
     });
